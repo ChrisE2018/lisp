ff40b38a2a8974204949672d94ae5d9228b2c8de
cre <cre@chriseliot.com>
2018-05-19 12:35:39 -0400

    Add error class

---
+++ lisp/src/lisp/eval/UnboundVariableError.java
@@ -0,0 +1,38 @@
+
+package lisp.eval;
+
+import lisp.Symbol;
+
+public class UnboundVariableError extends Error
+{
+    private final Symbol variable;
+
+    public UnboundVariableError (final Symbol variable)
+    {
+	super (String.format ("Unbound global %s", variable));
+	this.variable = variable;
+    }
+
+    public UnboundVariableError (final Symbol variable, final String format, final Object... args)
+    {
+	super (String.format (format, args));
+	this.variable = variable;
+    }
+
+    public Symbol getVariable ()
+    {
+	return variable;
+    }
+
+    // @Override
+    // public String toString ()
+    // {
+    // final StringBuilder buffer = new StringBuilder ();
+    // buffer.append ("#<");
+    // buffer.append (getClass ().getSimpleName ());
+    // buffer.append (" ");
+    // buffer.append (variable);
+    // buffer.append (">");
+    // return buffer.toString ();
+    // }
+}
