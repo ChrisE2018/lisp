4eeae566b3bad3a0ee4b5a8e92998a767429dfb5
cre <cre@chriseliot.com>
2018-06-22 10:51:19 -0400

    : Improve test coverage
    Description: Coverage of LispStream,LispInputStream

--- lisp/src/lisp/test/TestLispReader.java
+++ lisp/src/lisp/test/TestLispReader.java
@@ -1,9 +1,12 @@
 
 package lisp.test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import lisp.lang.*;
 import lisp.lang.Package;
@@ -52,5 +55,99 @@ class TestLispReader
     void testImports ()
     {
 	assertNotNull (lispReader.getImports ());
+    }
+
+    @Test
+    void testPeekEOF () throws IOException
+    {
+	final LispStream stream = new LispInputStream ("a");
+	stream.setEofThrows (false);
+	assertTrue (stream.peek ('a'));
+	assertTrue (stream.peek ('a'));
+	assertTrue (stream.tryChar ('a'));
+	assertFalse (stream.peek ('a'));
+    }
+
+    @Test
+    void testPeekEOF2 () throws IOException
+    {
+	final LispStream stream = new LispInputStream ("a");
+	stream.setEofThrows (false);
+	assertTrue (stream.peek ('a'));
+	assertTrue (stream.peek ('a'));
+	assertTrue (stream.tryChar ('a'));
+	assertFalse (stream.peek ('a'));
+
+	assertThrows (java.lang.IllegalArgumentException.class, new Executable ()
+	{
+	    @Override
+	    public void execute () throws Throwable
+	    {
+		stream.read ('a');
+	    }
+	});
+    }
+
+    @Test
+    void testSetEOF () throws IOException
+    {
+	final LispStream stream = new LispStream ()
+	{
+
+	    @Override
+	    public boolean eof ()
+	    {
+		return false;
+	    }
+
+	    @Override
+	    public void close () throws IOException
+	    {
+	    }
+
+	    @Override
+	    public char peek () throws IOException
+	    {
+		return 0;
+	    }
+
+	    @Override
+	    public boolean peek (final char expected) throws IOException
+	    {
+		return false;
+	    }
+
+	    @Override
+	    public boolean peek2 (final char expected) throws IOException
+	    {
+		return false;
+	    }
+
+	    @Override
+	    public char read () throws IOException
+	    {
+		return 0;
+	    }
+
+	    @Override
+	    public void read (final char expected) throws IOException
+	    {
+	    }
+
+	    @Override
+	    public boolean tryChar (final char expected) throws IOException
+	    {
+		return false;
+	    }
+	};
+	stream.setEofThrows (false);
+	assertFalse (stream.eof ());
+	stream.close ();
+	assertEquals (0, stream.peek ());
+	assertFalse (stream.peek ('x'));
+	assertFalse (stream.peek2 ('x'));
+	assertEquals (0, stream.read ());
+	stream.read ('x');
+	assertFalse (stream.tryChar ('x'));
     }
 }
